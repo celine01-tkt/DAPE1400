240f3c625055a28df7e7e81fc9ab345aa934d423
celine01-tkt <tktuong01@gmail.com>
2025-09-01 14:17:09 +0200

    brukes ikke int n=5 ... atring resultat"..." lengre fordi:  - Oppgaven har endret seg. Nå skal programmet lese to desimaltall fra brukeren og vise et regnestykke, ikke legge til et fast heltall (n=5) etterpå. - Regnestykket skal vises: i stedet for bare å vise "Resultatet ble 22.67", skal vi vise hele regnestykket. - vi vil ha dynamisk input: n var hardkodeeet som 5 i det gamle eksemplet. nå skal brukeren selv skrive inn begge tallene. dermed er det unødvendig å legge til en konstant.

--- src/modul2/desimaltallAddisjon.java
+++ src/modul2/desimaltallAddisjon.java
@@ -1,12 +1,21 @@
 package modul2;
 
+import javax.swing.*;
+
 public class desimaltallAddisjon {
     public static void main(String[] args) {
+        //Leser inn to desimaltall fra brukeren:
+        String førsteStr = JOptionPane.showInputDialog("Skriv inn første desimaltall");
+        String andreStr = JOptionPane.showInputDialog("Skriv inn andre desimaltall");
+
+        //Konverterer til double:
         double d1 = 13.7;
         double d2 = 3.97;
-        int n = 5;
-        double s1 = d1 + d2; //s1 er lik 17.67
-        double s2 = s1 + n; //s2 er lik 22.67
+
+        //Beregner summen:
+        double sum = d1 + d2;
+
+
         String resultat = "Resultatet ble " + s2;
         // Variabelen resultat inneholder teksten Resultatet ble 22.67
         System.out.println(resultat);
