0fb068e68e2035475fe56fd4087a990945e0d377
celine01-tkt <tktuong01@gmail.com>
2025-09-01 11:18:50 +0200

    Oppgave 4: datatype for desimaltall for double. her lag en ny versjon av addisjonprogrammet slik utskriften skal komme opp i oppgave 3, som vist på meldingsboksen.

---
+++ src/modul2/desimaltallAddisjon.java
@@ -0,0 +1,15 @@
+package modul2;
+
+public class desimaltallAddisjon {
+    public static void main(String[] args) {
+        double d1 = 13.7;
+        double d2 = 3.97;
+        int n = 5;
+        double s1 = d1 + d2; //s1 er lik 17.67
+        double s2 = s1 + n; //s2 er lik 22.67
+        String resultat = "Resultatet ble " + s2;
+        // Variabelen resultat inneholder teksten Resultatet ble 22.67
+        System.out.println(resultat);
+
+    }
+}
