859faad0ca0353dac3afd5bdccb899b3af78f5d5
celine01-tkt <tktuong01@gmail.com>
2025-11-30 22:01:57 +0100

    example 4 if statement

--- til eksamen/src/test.java
+++ til eksamen/src/test.java
@@ -1,9 +1,9 @@
 public class test {
     public static void main(String[] args) {
         int x = 20;
-        int y = 18;
-        if (x>y){
-            System.out.println("x is greater than y");
+        int y = 20;
+        if (x==y){
+            System.out.println("x is equal to y");
         }
     }
 }
