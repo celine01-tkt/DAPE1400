55660daf3661a0c6f87be354e44ff96897bb4f05
celine01-tkt <tktuong01@gmail.com>
2025-11-30 23:31:23 +0100

    example 6 if statement and using boolean variables with the value false, then the line sout runs no matter what

--- til eksamen/src/test.java
+++ til eksamen/src/test.java
@@ -1,9 +1,11 @@
 public class test {
     public static void main(String[] args) {
-        boolean isLightOn = true;
+        boolean isLightOn = false;
 
         if(isLightOn){
             System.out.println("The light is on");
         }
+
+        System.out.println("This line runs no matter what, because it is outside the if statement.");
     }
 }
