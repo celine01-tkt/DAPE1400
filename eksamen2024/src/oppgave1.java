3fd599d99fadc69f89f49bb3874d2423a9c972fa
celine01-tkt <tktuong01@gmail.com>
2025-12-01 15:14:29 +0100

    her er 4.punktet i oppgaven, skriv ut alle tall i arrayet som er delelig med number (hint: modulo %). number != 0 current tall % number  == 0

--- eksamen2024/src/oppgave1.java
+++ eksamen2024/src/oppgave1.java
@@ -7,13 +7,23 @@ public class oppgave1 {
         //summen av alle verdier
         int sum = 0;
 
+        //for hvert tall i arrayet
         for (int i = 0; i < ints.length; i++){
             int currentInt = ints[i];
 
             if (currentInt < 0){
                 mindreEnnNull++; //øker med 1
             }
+
+            //summen av alle verdier
             sum += currentInt; //sum = sum + currentInt
+
+            //skriv ut alle tall i arrayet ints som er delelig med number (modulo)
+            if (number != 0){
+                if(currentInt % number == 0){
+                    System.out.println(currentInt + " er delelig med" + number);
+                }
+            }
         }
         System.out.println("Mindre enn null: " + mindreEnnNull);
         System.out.println("Sum av alle verdier: " + sum);
