04a117bbf8df8b8a5dbc872f32b51813633656b7
celine01-tkt <tktuong01@gmail.com>
2025-12-01 04:00:53 +0100

    her sette jeg inn en array og fjerner (int number) i 2.linjer for den er en tomme metode her. jeg har også forkorte koden med bruke bare en forloop.

--- eksamen2024/src/oppgave1.java
+++ eksamen2024/src/oppgave1.java
@@ -1,23 +1,27 @@
 public class oppgave1 {
-    public void investigateIntArray(int[] ints, int number){
+    public void investigateIntArray(int[] ints){
+
         //ints som er mindre enn 0
         int mindreEnnNull = 0;
+
+        //summen av alle verdier
+        int sum = 0;
+
         for (int i = 0; i < ints.length; i++){
             int currentInt = ints[i];
 
             if (currentInt < 0){
                 mindreEnnNull++; //øker med 1
             }
-        }
-        System.out.println("Mindre enn null: " + mindreEnnNull);
-
-        //summen av alle verdier
-        int sum = 0;
-        for (int i = 0; i < ints.length; i++){
-            int currentInt = ints[i];
 
             sum += currentInt; //sum = sum + currentInt
         }
+        System.out.println("Mindre enn null: " + mindreEnnNull);
         System.out.println("Sum av alle verdier: " + sum);
+    }
+    public static void main(String[] args) {
+        int[] testArray = {3, -2, 0, -7, 5, -1};
+        oppgave1 oppgave1 = new oppgave1();
+        oppgave1.investigateIntArray(testArray);
     }
 }
