45fccda6394391fbafb266a462ffebd3589b6713
celine01-tkt <tktuong01@gmail.com>
2025-12-01 03:26:41 +0100

    her løser jeg oppgaven 1 med forloop, kan egt bruke 1 forloop

---
+++ eksamen2024/src/oppgave1.java
@@ -0,0 +1,23 @@
+public class oppgave1 {
+    public void investigateIntArray(int[] ints, int number){
+        //ints som er mindre enn 0
+        int mindreEnnNull = 0;
+        for (int i = 0; i < ints.length; i++){
+            int currentInt = ints[i];
+
+            if (currentInt < 0){
+                mindreEnnNull++; //øker med 1
+            }
+        }
+        System.out.println("Mindre enn null: " + mindreEnnNull);
+
+        //summen av alle verdier
+        int sum = 0;
+        for (int i = 0; i < ints.length; i++){
+            int currentInt = ints[i];
+
+            sum += currentInt; //sum = sum + currentInt
+        }
+        System.out.println("Sum av alle verdier: " + sum);
+    }
+}
