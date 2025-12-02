36880a65acac547555024d79d59d2bbe47a9da83
celine01-tkt <tktuong01@gmail.com>
2025-12-02 17:54:09 +0100

    oppgave 2

--- eksamen2024/src/oppgave2.java
+++ eksamen2024/src/oppgave2.java
@@ -1,2 +1,16 @@
 public class oppgave2 {
+    public static double stringDivision(String teller, String nevner) {
+        double result;
+        try {
+            int numberTeller = Integer.parseInt(teller);
+            int numberNevner = Integer.parseInt(nevner);
+            if (numberTeller < 0 || numberNevner <= 0) { //Avoiding neg number and dividing by 0
+                return -1;
+            }
+            result = (double) numberTeller / numberNevner; //converting to double to get a double result
+        } catch (Exception e) {
+            return -1;
+        }
+        return result;
+    }
 }
