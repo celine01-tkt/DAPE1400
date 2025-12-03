f67d7f1c8c6a1192a7247a1b9d822028dfb9cff7
celine01-tkt <tktuong01@gmail.com>
2025-12-03 15:05:50 +0100

    while lopp example. i is a counter variable, stands for index or iterator. i++ is a condition that used to increase the variabel, otherwise the loop is never end.

--- til eksamen/src/test.java
+++ til eksamen/src/test.java
@@ -1,11 +1,11 @@
 public class test {
-    public static void main(String[] args) {
-        boolean isRaining = false;
-
-        if(isRaining) {
-            System.out.println("Bring an umbrella");
-        }else {
-            System.out.println("No rain today, no need for an umbrella");
+    // use while loop that the code will run again and again, as long as variable (i) iss less than 5
+    public static void main (String[] args) {
+        int i = 0;
+        while (i < 5){
+            System.out.println(i);
+            i++;
         }
+
     }
 }
