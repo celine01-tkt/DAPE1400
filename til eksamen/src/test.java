814f7bd3c4afb82bfa0b6faa1e0e1314470fbbe4
celine01-tkt <tktuong01@gmail.com>
2025-12-03 17:20:45 +0100

    while lopp example. countdown-- is used to take out 1 from countdown, if not it will the while loop wil run again and again non-stop.

--- til eksamen/src/test.java
+++ til eksamen/src/test.java
@@ -1,11 +1,11 @@
 public class test {
     // use while loop that the code will run again and again, as long as variable (i) iss less than 5
     public static void main (String[] args) {
-        int i = 0;
-        while (i < 5){
-            System.out.println(i);
-            i++;
+        int countdown = 3;
+        while (countdown > 0){
+            System.out.println(countdown);
+            countdown--;
         }
-
+        System.out.println("Happy New Year!!");
     }
 }
