e04f86b0de0a3547c27bfc3ceee68f2aa1bb5b94
celine01-tkt <tktuong01@gmail.com>
2025-12-06 18:00:38 +0100

    oppgave 1 a) tell opp tall som er større enn 10

---
+++ eksamen2022/src/oppgave1.java
@@ -0,0 +1,17 @@
+public class oppgave1 {
+    public static void main(String[] args) {
+        //a)tall som er større enn 10
+        int[] liste = { -3, 41, 5, -3, 2, 49};
+        int teller = 0;
+
+        for (int i : liste){
+            if(i>10){
+                teller++;
+            }
+        }
+        System.out.println("Antall tall som er større enn 10 er: " + teller);
+
+        //b)annethvert element
+
+    }
+}
