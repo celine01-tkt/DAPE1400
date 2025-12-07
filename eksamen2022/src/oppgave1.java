113932a4fae352c99f1f387bd9e04adb198c451c
celine01-tkt <tktuong01@gmail.com>
2025-12-07 02:12:25 +0100

    oppgave 1 b,c,d)

--- eksamen2022/src/oppgave1.java
+++ eksamen2022/src/oppgave1.java
@@ -12,6 +12,27 @@ public class oppgave1 {
         System.out.println("Antall tall som er større enn 10 er: " + teller);
 
         //b)annethvert element
+        System.out.print("Annethvert tall: ");
+        for (int i = 0; i < liste.length; i += 2){
+            System.out.print(liste[i] + " ");
+        }
+        System.out.println();
 
+
+        //c)summen av alle positive tall
+        int sum = 0;
+        int antallPositive = 0;
+
+        for (int i : liste){
+            if( i > 0 ){
+                sum += i;
+                antallPositive++;
+            }
+        }
+        System.out.println("Summen av alle positive tall er: " + sum);
+
+        //d)gjennomsnittet av alle positive tall i arrayet
+        double gjennomsnitt = (double) sum / antallPositive;
+        System.out.println("Gjennomsnittet av alle de positive tallene er: "  + gjennomsnitt);
     }
 }
---
+++ eksamen2022/src/oppgave2.java
@@ -0,0 +1,2 @@
+public class oppgave2 {
+}
