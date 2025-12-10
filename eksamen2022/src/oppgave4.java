73e11c9b6bc501b6f9845cb0457a7529bd3e7dd3
celine01-tkt <tktuong01@gmail.com>
2025-12-10 03:03:12 +0100

    oppgave 4 bruk String.format("%.2f", c) is stedet for c slik blir det 2 tall etter komma, eks 37,78 (37,77777777...)

--- eksamen2022/src/oppgave4.java
+++ eksamen2022/src/oppgave4.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class oppgave4 {
     class KonverterTemperatur{
-        public static double tilCelsius(double fahrenheit){
+        public static double tilCelsius (double fahrenheit){
             return (fahrenheit - 32) / 1.8;
         }
 
-        public static double tilFahrenheiht(double celsius){
+        public static double tilFahrenheiht (double celsius){
             return celsius * 1.8 + 32;
         }
     }
@@ -12,10 +14,10 @@ public class oppgave4 {
     public static void main(String[] args) {
         double f = 100;
         double c = KonverterTemperatur.tilCelsius(f);
-        System.out.println(f + " Fahrenheit er " + c + " Celsius");
+        System.out.println(f + " Fahrenheit er " + String.format("%.2f",c) + " Celsius");
 
         double c2 = 25;
         double f2 = KonverterTemperatur.tilFahrenheiht(c2);
-        System.out.println(c2 + " Celsius er " + f2 + " Fahrenheit");
+        System.out.println(c2 + " Celsius er " + String.format("%.2f",f2) + " Fahrenheit");
     }
 }
